81f8ae3c23c9d9761579fc90daa49d2dc9e723c5
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-10 18:03:43 -0300

    novos exercicios
    (desafio16) #Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
    (desafio17) #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.
    (desafio18) #Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
    (desafio19) #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
    (desafio19b) #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele na escolha
    (desafio20) #O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.

---
+++ desafio19b.py
@@ -0,0 +1,10 @@
+#Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele na escolha
+import random
+num = random.randint(1, 4)
+print(num)
+print('Lista de participantes:')
+print('1 - Juvi')
+print('2 - Juão')
+print('3 - Kacashi')
+print('4 - Vitu')
+
