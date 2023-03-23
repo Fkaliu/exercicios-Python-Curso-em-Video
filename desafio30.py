1638792105e9506ffef9942d74f03287ca4b364c
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-23 06:35:24 -0300

    s

--- desafio30.py
+++
@@ -1,6 +0,0 @@
-#Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
-numero = int(input('Me diga um numero qualquer: '))
-if (numero % 2) == 0:
-    print('O numero {} é PAR'.format(numero))
-else:
-    print('O número {} é IMPAR'.format(numero))
