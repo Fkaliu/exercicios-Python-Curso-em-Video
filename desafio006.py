9abb135759c1ec8a6b637166767563547446324a
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-23 06:37:57 -0300

    s

--- desafio006.py
+++
@@ -1,8 +0,0 @@
-#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
-n = int(input('Digite um número '))
-d = n*2
-t = n*3
-r = n**(1/2)
-
-print('O número digitado é "{}", o dobro deste número é "{}"'.format(n, d), end=' ')
-print('o triplo deste número é "{}" e a raiz quadrada é "{:.3f}"!'.format(t, r))
