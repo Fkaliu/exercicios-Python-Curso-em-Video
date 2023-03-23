967cac40910b20b9a04ff9492f32b8dc469a1fd9
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-23 06:40:16 -0300

    s

--- desafio24.py
+++
@@ -1,5 +0,0 @@
-#Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
-cid = str(input('Em que cidade você nasceu? ')).strip()
-cidade = cid.upper()
-print('SANTO' in cidade)
-print(cidade[:5] == 'SANTO')
