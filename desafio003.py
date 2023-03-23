1a249a9b18d331edcf7367e0d1de47200538add9
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-23 06:37:24 -0300

    s

--- desafio003.py
+++
@@ -1,5 +0,0 @@
-#Desafio 003 = Crie um programa que leia dois numeros e mostre a soma entre eles
-n1 = int(input('Digite o primeiro numero: '))
-n2 = int(input('Digite o segundo numero: '))
-soma = n1 + n2
-print('A soma entre "{}" e "{}" é igual a "{}"'.format(n1,n2,soma))
