c72b1ecd5977ef941e67118c1bcbb439dc3a89b4
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-23 06:33:37 -0300

    s

--- calculo desconto.py
+++
@@ -1,5 +0,0 @@
-#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
-n1 = float(input('Informe o valor do produto: R$ '))
-des = n1 - (n1 * 5 / 100)
-print('O valor do produto é: R${:.2f}, com o desconto aplicado fica por: RS{:.2f}'.format(n1, des))
-
