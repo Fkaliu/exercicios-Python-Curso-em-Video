03c0dfbb5f00a50b9cf6bbca17cd9ebd679f768d
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-23 06:37:49 -0300

    s

--- desafio002.py
+++
@@ -1,5 +0,0 @@
-#Um script que leia dois numeros e tente mostrar a soma entre eles.
-num1 = input('Primeiro número ')
-num2 = input('Segundo número ')
-soma = int(num1) + int(num2)
-print('A soma é ',soma)
