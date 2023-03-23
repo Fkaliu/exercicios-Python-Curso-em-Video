e0a07558a1fa6c718d78dc1e7444edbc1adb98a7
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-23 06:33:49 -0300

    s

--- conversor moeda.py
+++
@@ -1,4 +0,0 @@
-#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
-n = float(input('Quanto dinheiro você tem no momento? R$ '))
-dou = n*5.50
-print('Voce tem {} em dolares disponiveis'.format(dou))
