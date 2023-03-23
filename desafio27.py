6c0c4b889a266d5a01cb2e48d1cfeb16a0c98f4e
Filipe Kaliu <48028612+Fkaliu@users.noreply.github.com>
2023-03-23 06:39:09 -0300

    s

--- desafio27.py
+++
@@ -1,7 +0,0 @@
-#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
-nome = str(input('Digite o seu nome completo: ')).upper().strip()
-separado = nome.split()
-print(separado)
-print('Seu primeiro nome é {}'.format(separado[0]))
-print('Seu ultimo nome é {}'.format(separado[-1]))#boa
-
